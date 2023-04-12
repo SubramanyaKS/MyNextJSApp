437bfb6b20a8950950ffd114a9b6094ba9baf57b
Subramanya K S <subramanyaks2205@gmail.com>
2023-04-12 22:47:49 +0530

    updated navbar using tailwind css

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <h4><b>&copy; Subramanya KS</b></h4>
+      </footer>
+  )
+}
+
+export default Footer;
